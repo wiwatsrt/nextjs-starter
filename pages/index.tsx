627ff7f0ecd403732ec6e2a75c4980b55d8ce345
Wiwat Srisattha <wiwat_srt@hotmail.com>
2020-11-22 18:33:34 +0700

    fix: change header title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppLayout from '../components/Layout/AppLayout'
 
 export const Home = (): JSX.Element => (
-  <AppLayout header="Home">
+  <AppLayout headerTitle="Home">
     <div className="py-4">
       <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
     </div>
