c60a5b55b89ab281eef90e48c25cbae196631bef
Wiwat Srisattha <wiwat_srt@hotmail.com>
2022-12-27 22:22:52 +0700

    chore: updated nexts 13

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
+import { render, fireEvent } from '@testing-library/react'
 import { Home } from '../../pages/index'
 
 describe('Home page', () => {
